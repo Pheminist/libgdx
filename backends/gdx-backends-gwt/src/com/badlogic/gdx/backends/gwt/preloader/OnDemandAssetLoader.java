675a92c48867a7049d5231247b291e6bc4841d7c
SimonIT <simonit.orig@gmail.com>
2019-06-06 11:28:33 +0200

    spaces to tabs

--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/preloader/OnDemandAssetLoader.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/preloader/OnDemandAssetLoader.java
@@ -3,7 +3,6 @@ package com.badlogic.gdx.backends.gwt.preloader;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.OndemandAssetManager;
-import com.badlogic.gdx.assets.loaders.AssetLoader;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
@@ -15,146 +14,146 @@ import com.google.gwt.dom.client.ImageElement;
 
 public class OnDemandAssetLoader implements Preloader {
 
-    private final AssetDownloader loader;
-    private final PreloadedAssetManager preloadedAssetManager = new PreloadedAssetManager();
+	private final AssetDownloader loader;
+	private final PreloadedAssetManager preloadedAssetManager = new PreloadedAssetManager();
 
-    private final String baseUrl;
-    private final boolean crossDomain;
+	private final String baseUrl;
+	private final boolean crossDomain;
 
-    public OnDemandAssetLoader(
-            String baseUrl, boolean crossDomain) {
-        this.baseUrl = baseUrl;
-        this.crossDomain = crossDomain;
-        this.loader = crossDomain ? AssetDownloader.crossDomain() : AssetDownloader.standard();
-    }
+	public OnDemandAssetLoader(
+			String baseUrl, boolean crossDomain) {
+		this.baseUrl = baseUrl;
+		this.crossDomain = crossDomain;
+		this.loader = crossDomain ? AssetDownloader.crossDomain() : AssetDownloader.standard();
+	}
 
-    @Override
-    public void preload(final PreloaderCallback callback) {
+	@Override
+	public void preload(final PreloaderCallback callback) {
 
-        callback.update(new PreloaderState(new Array<Asset>()) {
-            @Override
-            public boolean hasEnded() {
-                return true;
-            }
-        });
-    }
+		callback.update(new PreloaderState(new Array<Asset>()) {
+			@Override
+			public boolean hasEnded() {
+				return true;
+			}
+		});
+	}
 
-    @Override
-    public PreloadedAssetManager getPreloadedAssetManager() {
-        return preloadedAssetManager;
-    }
+	@Override
+	public PreloadedAssetManager getPreloadedAssetManager() {
+		return preloadedAssetManager;
+	}
 
-    public void load(AssetDescriptor assetDescriptor, OndemandAssetManager.RetrievalListener retrievalListener) {
-        Gdx.app.debug("LOADING", "Downloading from " + (baseUrl + assetDescriptor.fileName));
+	public void load(AssetDescriptor assetDescriptor, OndemandAssetManager.RetrievalListener retrievalListener) {
+		Gdx.app.debug("LOADING", "Downloading from " + (baseUrl + assetDescriptor.fileName));
 
-        if (assetDescriptor.type == Texture.class) {
-            loadImage(assetDescriptor, retrievalListener);
-        }
-        if (assetDescriptor.type == TextureAtlas.class) {
-            loadAtlas(assetDescriptor, retrievalListener);
-        }
-        if (assetDescriptor.type == BitmapFont.class) {
-            loadBitmapFont(assetDescriptor, retrievalListener);
-        }
-        if (assetDescriptor.type == Music.class) {
-            loadMusic(assetDescriptor, retrievalListener);
-        }
+		if (assetDescriptor.type == Texture.class) {
+			loadImage(assetDescriptor, retrievalListener);
+		}
+		if (assetDescriptor.type == TextureAtlas.class) {
+			loadAtlas(assetDescriptor, retrievalListener);
+		}
+		if (assetDescriptor.type == BitmapFont.class) {
+			loadBitmapFont(assetDescriptor, retrievalListener);
+		}
+		if (assetDescriptor.type == Music.class) {
+			loadMusic(assetDescriptor, retrievalListener);
+		}
 
-        if (assetDescriptor.type == TiledMap.class) {
-            loadTileMap(assetDescriptor, retrievalListener);
-        }
+		if (assetDescriptor.type == TiledMap.class) {
+			loadTileMap(assetDescriptor, retrievalListener);
+		}
 
-        if (assetDescriptor.type == Sound.class) {
-            loadMusic(assetDescriptor, retrievalListener);
-        }
-    }
+		if (assetDescriptor.type == Sound.class) {
+			loadMusic(assetDescriptor, retrievalListener);
+		}
+	}
 
-    private void loadMusic(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
-        load(assetDescriptor, AssetFilter.AssetType.Audio, "", retrievalListener);
-    }
+	private void loadMusic(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
+		load(assetDescriptor, AssetFilter.AssetType.Audio, "", retrievalListener);
+	}
 
-    private void loadAtlas(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
-        load(assetDescriptor, AssetFilter.AssetType.Text, "text/plain", retrievalListener);
-    }
+	private void loadAtlas(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
+		load(assetDescriptor, AssetFilter.AssetType.Text, "text/plain", retrievalListener);
+	}
 
-    private void loadBitmapFont(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
-        load(assetDescriptor, AssetFilter.AssetType.Text, "application/unknown", retrievalListener);
-    }
+	private void loadBitmapFont(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
+		load(assetDescriptor, AssetFilter.AssetType.Text, "application/unknown", retrievalListener);
+	}
 
-    private void loadImage(final AssetDescriptor assetDescriptor, OndemandAssetManager.RetrievalListener retrievalListener) {
-        String assetFileUrl = assetDescriptor.fileName;
-        String mimeType = assetFileUrl.endsWith("png") ? "image/png" : "image/jpeg";
-        load(assetDescriptor, AssetFilter.AssetType.Image, mimeType, retrievalListener);
-    }
+	private void loadImage(final AssetDescriptor assetDescriptor, OndemandAssetManager.RetrievalListener retrievalListener) {
+		String assetFileUrl = assetDescriptor.fileName;
+		String mimeType = assetFileUrl.endsWith("png") ? "image/png" : "image/jpeg";
+		load(assetDescriptor, AssetFilter.AssetType.Image, mimeType, retrievalListener);
+	}
 
-    private void loadTileMap(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
-        load(assetDescriptor, AssetFilter.AssetType.Text, "text/plain", retrievalListener);
-    }
+	private void loadTileMap(final AssetDescriptor assetDescriptor, final OndemandAssetManager.RetrievalListener retrievalListener) {
+		load(assetDescriptor, AssetFilter.AssetType.Text, "text/plain", retrievalListener);
+	}
 
-    private void load(final AssetDescriptor assetDescriptor, final AssetFilter.AssetType assetType, String mimeType, final OndemandAssetManager.RetrievalListener retrievalListener) {
-        load(baseUrl, assetDescriptor, assetType, mimeType, retrievalListener);
-    }
+	private void load(final AssetDescriptor assetDescriptor, final AssetFilter.AssetType assetType, String mimeType, final OndemandAssetManager.RetrievalListener retrievalListener) {
+		load(baseUrl, assetDescriptor, assetType, mimeType, retrievalListener);
+	}
 
-    public void load(final String baseUrl, final AssetDescriptor assetDescriptor, final AssetFilter.AssetType assetType, String mimeType, final OndemandAssetManager.RetrievalListener retrievalListener) {
-        loader.load(baseUrl + assetDescriptor.fileName, assetType, mimeType, new AssetDownloader.AssetLoaderListener<Object>() {
-            @Override
-            public void onProgress(double amount) {
-                retrievalListener.onProgress(assetDescriptor.fileName, assetDescriptor.type, (float) amount);
-            }
+	public void load(final String baseUrl, final AssetDescriptor assetDescriptor, final AssetFilter.AssetType assetType, String mimeType, final OndemandAssetManager.RetrievalListener retrievalListener) {
+		loader.load(baseUrl + assetDescriptor.fileName, assetType, mimeType, new AssetDownloader.AssetLoaderListener<Object>() {
+			@Override
+			public void onProgress(double amount) {
+				retrievalListener.onProgress(assetDescriptor.fileName, assetDescriptor.type, (float) amount);
+			}
 
-            @Override
-            public void onFailure() {
-                retrievalListener.onFailure(assetDescriptor.fileName, assetDescriptor.type);
-            }
+			@Override
+			public void onFailure() {
+				retrievalListener.onFailure(assetDescriptor.fileName, assetDescriptor.type);
+			}
 
-            @Override
-            public void onSuccess(Object result) {
-                switch (assetType) {
-                    case Text:
-                        preloadedAssetManager.texts.put(assetDescriptor.fileName, (String) result);
-                        break;
-                    case Image: {
-                        ImageElement imageElement = (ImageElement) result;
-                        if (crossDomain) {
-                            Gdx.app.debug("LOADING", "SETTING CROSS DOMAIN");
-                            imageElement.setAttribute("crossOrigin", AssetDownloader.CROSS_DOMAIN);
-                        }
-                        preloadedAssetManager.images.put(assetDescriptor.fileName, (ImageElement) result);
-                    }
-                    break;
-                    case Binary:
-                        preloadedAssetManager.binaries.put(assetDescriptor.fileName, (Blob) result);
-                        break;
-                    case Audio:
-                        preloadedAssetManager.audio.put(assetDescriptor.fileName, null);
-                        break;
-                    case Directory:
-                        preloadedAssetManager.directories.put(assetDescriptor.fileName, null);
-                        break;
-                }
-                retrievalListener.onSuccess(assetDescriptor.fileName, assetDescriptor.type);
-            }
-        });
-    }
+			@Override
+			public void onSuccess(Object result) {
+				switch (assetType) {
+					case Text:
+						preloadedAssetManager.texts.put(assetDescriptor.fileName, (String) result);
+						break;
+					case Image: {
+						ImageElement imageElement = (ImageElement) result;
+						if (crossDomain) {
+							Gdx.app.debug("LOADING", "SETTING CROSS DOMAIN");
+							imageElement.setAttribute("crossOrigin", AssetDownloader.CROSS_DOMAIN);
+						}
+						preloadedAssetManager.images.put(assetDescriptor.fileName, (ImageElement) result);
+					}
+					break;
+					case Binary:
+						preloadedAssetManager.binaries.put(assetDescriptor.fileName, (Blob) result);
+						break;
+					case Audio:
+						preloadedAssetManager.audio.put(assetDescriptor.fileName, null);
+						break;
+					case Directory:
+						preloadedAssetManager.directories.put(assetDescriptor.fileName, null);
+						break;
+				}
+				retrievalListener.onSuccess(assetDescriptor.fileName, assetDescriptor.type);
+			}
+		});
+	}
 
-    public void loadAbsoluteCrossDomain(final AssetDescriptor assetDescriptor, String mimeType, final OndemandAssetManager.RetrievalListener retrievalListener) {
-        loader.loadImage(assetDescriptor.fileName, mimeType, AssetDownloader.CROSS_DOMAIN, new AssetDownloader.AssetLoaderListener<ImageElement>() {
-            @Override
-            public void onProgress(double amount) {
-                retrievalListener.onProgress(assetDescriptor.fileName, assetDescriptor.type, (float) amount);
-            }
+	public void loadAbsoluteCrossDomain(final AssetDescriptor assetDescriptor, String mimeType, final OndemandAssetManager.RetrievalListener retrievalListener) {
+		loader.loadImage(assetDescriptor.fileName, mimeType, AssetDownloader.CROSS_DOMAIN, new AssetDownloader.AssetLoaderListener<ImageElement>() {
+			@Override
+			public void onProgress(double amount) {
+				retrievalListener.onProgress(assetDescriptor.fileName, assetDescriptor.type, (float) amount);
+			}
 
-            @Override
-            public void onFailure() {
-                retrievalListener.onFailure(assetDescriptor.fileName, assetDescriptor.type);
-            }
+			@Override
+			public void onFailure() {
+				retrievalListener.onFailure(assetDescriptor.fileName, assetDescriptor.type);
+			}
 
-            @Override
-            public void onSuccess(ImageElement result) {
-                preloadedAssetManager.images.put(assetDescriptor.fileName, result);
-                retrievalListener.onSuccess(assetDescriptor.fileName, assetDescriptor.type);
-            }
-        });
-    }
+			@Override
+			public void onSuccess(ImageElement result) {
+				preloadedAssetManager.images.put(assetDescriptor.fileName, result);
+				retrievalListener.onSuccess(assetDescriptor.fileName, assetDescriptor.type);
+			}
+		});
+	}
 
 }
--- backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/preloader/PreloadedAssetManager.java
+++ backends/gdx-backends-gwt/src/com/badlogic/gdx/backends/gwt/preloader/PreloadedAssetManager.java
@@ -27,125 +27,125 @@ import java.io.*;
 
 public class PreloadedAssetManager {
 
-    public ObjectMap<String, Void> directories = new ObjectMap<String, Void>();
-    public ObjectMap<String, ImageElement> images = new ObjectMap<String, ImageElement>();
-    public ObjectMap<String, Void> audio = new ObjectMap<String, Void>();
-    public ObjectMap<String, String> texts = new ObjectMap<String, String>();
-    public ObjectMap<String, Blob> binaries = new ObjectMap<String, Blob>();
+	public ObjectMap<String, Void> directories = new ObjectMap<String, Void>();
+	public ObjectMap<String, ImageElement> images = new ObjectMap<String, ImageElement>();
+	public ObjectMap<String, Void> audio = new ObjectMap<String, Void>();
+	public ObjectMap<String, String> texts = new ObjectMap<String, String>();
+	public ObjectMap<String, Blob> binaries = new ObjectMap<String, Blob>();
 
-    public InputStream read (String url) {
-        if (texts.containsKey(url)) {
-            try {
-                return new ByteArrayInputStream(texts.get(url).getBytes("UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                return null;
-            }
-        }
-        if (images.containsKey(url)) {
-            return new ByteArrayInputStream(new byte[1]); // FIXME, sensible?
-        }
-        if (binaries.containsKey(url)) {
-            return binaries.get(url).read();
-        }
-        if (audio.containsKey(url)) {
-            return new ByteArrayInputStream(new byte[1]); // FIXME, sensible?
-        }
-        return null;
-    }
+	public InputStream read(String url) {
+		if (texts.containsKey(url)) {
+			try {
+				return new ByteArrayInputStream(texts.get(url).getBytes("UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				return null;
+			}
+		}
+		if (images.containsKey(url)) {
+			return new ByteArrayInputStream(new byte[1]); // FIXME, sensible?
+		}
+		if (binaries.containsKey(url)) {
+			return binaries.get(url).read();
+		}
+		if (audio.containsKey(url)) {
+			return new ByteArrayInputStream(new byte[1]); // FIXME, sensible?
+		}
+		return null;
+	}
 
-    public boolean contains (String url) {
-        return texts.containsKey(url) || images.containsKey(url) || binaries.containsKey(url) || audio.containsKey(url) || directories.containsKey(url);
-    }
+	public boolean contains(String url) {
+		return texts.containsKey(url) || images.containsKey(url) || binaries.containsKey(url) || audio.containsKey(url) || directories.containsKey(url);
+	}
 
-    public boolean isText (String url) {
-        return texts.containsKey(url);
-    }
+	public boolean isText(String url) {
+		return texts.containsKey(url);
+	}
 
-    public boolean isImage (String url) {
-        return images.containsKey(url);
-    }
+	public boolean isImage(String url) {
+		return images.containsKey(url);
+	}
 
-    public boolean isBinary (String url) {
-        return binaries.containsKey(url);
-    }
+	public boolean isBinary(String url) {
+		return binaries.containsKey(url);
+	}
 
-    public boolean isAudio (String url) {
-        return audio.containsKey(url);
-    }
+	public boolean isAudio(String url) {
+		return audio.containsKey(url);
+	}
 
-    public boolean isDirectory (String url) {
-        return directories.containsKey(url);
-    }
+	public boolean isDirectory(String url) {
+		return directories.containsKey(url);
+	}
 
-    private boolean isChild(String path, String url) {
-        return path.startsWith(url) && (path.indexOf('/', url.length() + 1) < 0);
-    }
+	private boolean isChild(String path, String url) {
+		return path.startsWith(url) && (path.indexOf('/', url.length() + 1) < 0);
+	}
 
-    public FileHandle[] list (String url) {
-        Array<FileHandle> files = new Array<FileHandle>();
-        for (String path : texts.keys()) {
-            if (isChild(path, url)) {
-                files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
-            }
-        }
-        FileHandle[] list = new FileHandle[files.size];
-        System.arraycopy(files.items, 0, list, 0, list.length);
-        return list;
-    }
+	public FileHandle[] list(String url) {
+		Array<FileHandle> files = new Array<FileHandle>();
+		for (String path : texts.keys()) {
+			if (isChild(path, url)) {
+				files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
+			}
+		}
+		FileHandle[] list = new FileHandle[files.size];
+		System.arraycopy(files.items, 0, list, 0, list.length);
+		return list;
+	}
 
-    public FileHandle[] list (String url, FileFilter filter) {
-        Array<FileHandle> files = new Array<FileHandle>();
-        for (String path : texts.keys()) {
-            if (isChild(path, url) && filter.accept(new File(path))) {
-                files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
-            }
-        }
-        FileHandle[] list = new FileHandle[files.size];
-        System.arraycopy(files.items, 0, list, 0, list.length);
-        return list;
-    }
+	public FileHandle[] list(String url, FileFilter filter) {
+		Array<FileHandle> files = new Array<FileHandle>();
+		for (String path : texts.keys()) {
+			if (isChild(path, url) && filter.accept(new File(path))) {
+				files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
+			}
+		}
+		FileHandle[] list = new FileHandle[files.size];
+		System.arraycopy(files.items, 0, list, 0, list.length);
+		return list;
+	}
 
-    public FileHandle[] list (String url, FilenameFilter filter) {
-        Array<FileHandle> files = new Array<FileHandle>();
-        for (String path : texts.keys()) {
-            if (isChild(path, url) && filter.accept(new File(url), path.substring(url.length() + 1))) {
-                files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
-            }
-        }
-        FileHandle[] list = new FileHandle[files.size];
-        System.arraycopy(files.items, 0, list, 0, list.length);
-        return list;
-    }
+	public FileHandle[] list(String url, FilenameFilter filter) {
+		Array<FileHandle> files = new Array<FileHandle>();
+		for (String path : texts.keys()) {
+			if (isChild(path, url) && filter.accept(new File(url), path.substring(url.length() + 1))) {
+				files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
+			}
+		}
+		FileHandle[] list = new FileHandle[files.size];
+		System.arraycopy(files.items, 0, list, 0, list.length);
+		return list;
+	}
 
-    public FileHandle[] list (String url, String suffix) {
-        Array<FileHandle> files = new Array<FileHandle>();
-        for (String path : texts.keys()) {
-            if (isChild(path, url) && path.endsWith(suffix)) {
-                files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
-            }
-        }
-        FileHandle[] list = new FileHandle[files.size];
-        System.arraycopy(files.items, 0, list, 0, list.length);
-        return list;
-    }
+	public FileHandle[] list(String url, String suffix) {
+		Array<FileHandle> files = new Array<FileHandle>();
+		for (String path : texts.keys()) {
+			if (isChild(path, url) && path.endsWith(suffix)) {
+				files.add(new GwtFileHandle(this, path, Files.FileType.Internal));
+			}
+		}
+		FileHandle[] list = new FileHandle[files.size];
+		System.arraycopy(files.items, 0, list, 0, list.length);
+		return list;
+	}
 
-    public long length (String url) {
-        if (texts.containsKey(url)) {
-            try {
-                return texts.get(url).getBytes("UTF-8").length;
-            } catch (UnsupportedEncodingException e) {
-                return texts.get(url).getBytes().length;
-            }
-        }
-        if (images.containsKey(url)) {
-            return 1; // FIXME, sensible?
-        }
-        if (binaries.containsKey(url)) {
-            return binaries.get(url).length();
-        }
-        if (audio.containsKey(url)) {
-            return 1; // FIXME sensible?
-        }
-        return 0;
-    }
+	public long length(String url) {
+		if (texts.containsKey(url)) {
+			try {
+				return texts.get(url).getBytes("UTF-8").length;
+			} catch (UnsupportedEncodingException e) {
+				return texts.get(url).getBytes().length;
+			}
+		}
+		if (images.containsKey(url)) {
+			return 1; // FIXME, sensible?
+		}
+		if (binaries.containsKey(url)) {
+			return binaries.get(url).length();
+		}
+		if (audio.containsKey(url)) {
+			return 1; // FIXME sensible?
+		}
+		return 0;
+	}
 }
